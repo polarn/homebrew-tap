25fbbd8f463f278e35f5b025e9f45b2d1231338b
Marcus Johansson <polarn@gmail.com>
2025-10-30 23:36:57 +0100

    Remove old Formula, use Cask instead

--- Formula/env-exec.rb
+++
@@ -1,8 +0,0 @@
-class EnvExec < Formula
-desc "DEPRECATED: Use 'brew install polarn/tap/env-exec' (Cask) instead"
-homepage "https://github.com/polarn/env-exec"
-
-def install
-    odie "This formula is deprecated. Please use: brew install polarn/tap/env-exec"
-end
-end
